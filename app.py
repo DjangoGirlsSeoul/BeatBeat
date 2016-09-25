30a36ad7921ffde7117603876bfafa2e5086843c
Hassan Abid <hassanabidpk89@gmail.com>
2016-09-25 13:25:28 +0900

    add speech api code

--- app.py
+++
@@ -1,9 +0,0 @@
-import soundcloud
-
-QUERY = 'girlsgeneration'
-CLIENT_ID = '' 
-client = soundcloud.Client(client_id=CLIENT_ID)
-tracks = client.get('/tracks', q=QUERY, order='hotness', limit=1)
-for track in tracks:
-  print track.stream_url
-
